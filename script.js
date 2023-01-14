359afaeb12bcbee269c66028c99800bef9db6135
Thiana <thianacantunes@gmail.com>
2023-01-14 16:04:32 -0300

    VERSÃO FINAL PROJETO PARROTS

--- script.js
+++ script.js
@@ -1,21 +1,15 @@
 
-
 let qtdCartas = prompt("Com quantas cartas quer jogar?");
-
 let gifs = [1, 2, 3, 4, 5, 6, 7].sort(() => Math.random() - 0.5);
-
 let carta1 = "";
 let carta2 = "";
-
 let jogadas = 0;
 let tempo = 0;
 let idIntervalo;
 
 
 criarCarta();
-contar ();
-
-
+contar();
 
 function criarCarta() {
 
@@ -24,7 +18,6 @@ function criarCarta() {
   for (let i = 0; i < qtdCartas / 2; i++) {
 
     cardsId.push(gifs[i]);
-
   }
 
   let cardsDuplicadas = ([...cardsId, ...cardsId]).sort(() => Math.random() - 0.5);
@@ -37,22 +30,20 @@ function criarCarta() {
   while (contador < qtdCartas) {
 
     document.querySelector(".container").innerHTML += `
-    <div onclick="virarCarta(this)" class="card">
+    <div data-test="card" onclick="virarCarta(this)" class="card">
     <div class="front-face face">
-    <img src="./imagens/foto${cardsDuplicadas[contador]}.gif" class="imgFront">
+    <img data-test="face-up-image" src="./imagens/foto${cardsDuplicadas[contador]}.gif" class="imgFront">
     </div>
     <div class="back-face face">
-    <img src="./imagens/back.png" />
+    <img data-test="face-down-image" src="./imagens/back.png" />
     </div>
   </div>`
     contador++;
   }
-
-} 
-
+}
 
 function virarCarta(botaonoThis) {
- 
+
   if (botaonoThis.classList.contains("revelarcarta")) {
     return
   }
@@ -69,20 +60,18 @@ function virarCarta(botaonoThis) {
   }
 
   jogadas++
-  
 }
 
-
 function verificarCarta(cartaVirada) {
-  
+
 
   let cartaRevelada = document.querySelector('.imgFront').getAttribute('src');
-  
+
   if (carta1.querySelector('.imgFront').getAttribute('src') === carta2.querySelector('.imgFront').getAttribute('src')) {
-    
+
     carta1 = "";
     carta2 = "";
-    
+
   } else {
     setTimeout(() => {
       carta1.classList.remove("revelarcarta");
@@ -95,62 +84,45 @@ function verificarCarta(cartaVirada) {
   }
 }
 
-
-
-function endgame(){
+function endgame() {
 
   let qtdCartasViradas = document.querySelectorAll('.revelarcarta').length;
 
-    if (Number(qtdCartas) === qtdCartasViradas ){
+  if (Number(qtdCartas) === qtdCartasViradas) {
 
-      setTimeout(() => {
-        
-        alert (`Você ganhou em ${jogadas} jogadas! A duração do jogo foi ${tempo} de segundos!`);
-        clearInterval(idIntervalo);
-        
-        let resposta = prompt('Você gostaria de reiniciar a partida? sim ou não?');
+    setTimeout(() => {
 
-        let sim = 'sim';
+      alert(`Você ganhou em ${jogadas} jogadas! A duração do jogo foi ${tempo} de segundos!`);
+      clearInterval(idIntervalo);
 
-        if (resposta === 'sim'){
+      let resposta = prompt('Você gostaria de reiniciar a partida? sim ou não?');
 
-          atualizar();
-        } 
-        
+      let sim = 'sim';
 
-      }, 1000)
+      if (resposta === 'sim') {
 
-    
-}
-}
+        atualizar();
+      }
 
 
+    }, 1000)
 
-
-function contar(){
-
-
- idIntervalo = setInterval(acrescentarContador, 1000);
-  
+  }
 }
 
-function acrescentarContador(){
-  
+function contar() {
+
+  idIntervalo = setInterval(acrescentarContador, 1000);
+}
+
+function acrescentarContador() {
+
   tempo++;
 
   const elementoContador = document.querySelector(".contador");
   elementoContador.innerHTML = tempo;
+}
 
-  
-  }
-
-
-  function atualizar (){
-    window.location.reload();
-  }
-
-
-
-
-
-
+function atualizar() {
+  window.location.reload();
+}
